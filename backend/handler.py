51c8fdb5630d78576f0562c6cde6e20c573dadca
Nick Paz <nicholasmpaz@gmail.com>
2020-07-12 13:13:26 -0700

    work on backend

--- backend/handler.py
+++ backend/handler.py
@@ -1,12 +1,23 @@
 from http import HTTPStatus
 import json
+
+from .auth import verify_jwt
 from .utils import make_response, Message
 
 
 def ping(event, context):
     return make_response(message=Message.ping)
-    
+
+def ping_auth(event, context):
+    pass
+
+def get_notes(event, context):
+    pass
+
 def create_note(event, context):
+    pass
+
+def get_note(event, context):
     pass
 
 def update_note(event, context):
@@ -15,5 +26,3 @@ def update_note(event, context):
 def destroy_note(event, context):
     pass
 
-def get_notes(event, context):
-    pass
