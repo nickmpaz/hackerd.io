115b95178ba02b717d49457aff0cb32987e99688
Nicholas Paz <nicholasmpaz@gmail.com>
2020-08-04 17:26:45 -0700

    change backend verbage - note -> resource

--- backend/handler.py
+++ backend/handler.py
@@ -9,7 +9,7 @@ from boto3.dynamodb.conditions import Key, Attr
 
 logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
 dynamodb = boto3.resource('dynamodb')
-notes_table = dynamodb.Table('dolphin_notes_table')
+resources_table = dynamodb.Table('dolphin_resources_table')
 
 def _make_response(body={}, status_code=HTTPStatus.OK):
     response = {
@@ -22,18 +22,18 @@ def _make_response(body={}, status_code=HTTPStatus.OK):
     return response
 
 
-def _generate_note_id():
-    note_id = uuid.uuid4().hex
+def _generate_resource_id():
+    resource_id = uuid.uuid4().hex
     # limit tries to 100
     for i in range(100):
-        response = notes_table.scan(
-            FilterExpression=Attr('note_id').eq(note_id)
+        response = resources_table.scan(
+            FilterExpression=Attr('resource_id').eq(resource_id)
         )
         items = response['Items']
         if len(items) == 0:
-            return note_id
+            return resource_id
 
-        note_id = uuid.uuid4().hex
+        resource_id = uuid.uuid4().hex
 
     # FIXME write a more detailed exception
     raise Exception
@@ -49,29 +49,29 @@ def ping_auth(event, context):
     return _make_response(body=claims)
 
 
-def get_notes(event, context):
+def get_resources(event, context):
     user_id = event['requestContext']['authorizer']['claims']['sub']
 
-    response = notes_table.query(
+    response = resources_table.query(
         KeyConditionExpression=Key('user_id').eq(user_id)
     )
-    notes = response['Items']
-    print(notes)
-    return _make_response(body={'notes': notes})
+    resources = response['Items']
+    print(resources)
+    return _make_response(body={'resources': resources})
 
 
 
-def create_note(event, context):
+def create_resource(event, context):
     user_id = event['requestContext']['authorizer']['claims']['sub']
-    note_id = _generate_note_id()
-    note_type = json.loads(event['body']).get('note_type', 'markdown')
+    resource_id = _generate_resource_id()
+    resource_type = json.loads(event['body']).get('resource_type', 'markdown')
 
-    # create the note
-    notes_table.put_item(
+    # create the resource
+    resources_table.put_item(
         Item={
             'user_id': user_id,
-            'note_id': note_id,
-            'type': note_type,
+            'resource_id': resource_id,
+            'type': resource_type,
             'title': "Untitled",
             'tags': [],
             'document': "There's nothing here yet.",
@@ -79,48 +79,48 @@ def create_note(event, context):
             'updated_at': str(int(time.time()))
         }
     )
-    return _make_response(body={"note_id": note_id})
+    return _make_response(body={"resource_id": resource_id})
 
 
-def get_note(event, context):
+def get_resource(event, context):
     user_id = event['requestContext']['authorizer']['claims']['sub']
-    note_id = event['pathParameters']['note_id']
+    resource_id = event['pathParameters']['resource_id']
 
-    response = notes_table.get_item(
+    response = resources_table.get_item(
         Key={
             'user_id': user_id,
-            'note_id': note_id
+            'resource_id': resource_id
         }
     )
-    note = response.get('Item')
+    resource = response.get('Item')
 
-    if note is None:
+    if resource is None:
         return _make_response(status_code=HTTPStatus.NOT_FOUND)
 
-    return _make_response(body={'note': note})
+    return _make_response(body={'resource': resource})
 
 
-def update_note(event, context):
+def update_resource(event, context):
     user_id = event['requestContext']['authorizer']['claims']['sub']
-    note_id = event['pathParameters']['note_id']
-    note = json.loads(event['body']).get('note')
+    resource_id = event['pathParameters']['resource_id']
+    resource = json.loads(event['body']).get('resource')
     updated_at = str(int(time.time()))
-    note['updated_at'] = updated_at
+    resource['updated_at'] = updated_at
 
-    # make sure that user_id in note matches claims
-    if note.get('user_id') != user_id:
+    # make sure that user_id in resource matches claims
+    if resource.get('user_id') != user_id:
         return _make_response(status_code=HTTPStatus.FORBIDDEN)
 
-    notes_table.put_item(Item=note)
+    resources_table.put_item(Item=resource)
     return _make_response(body={'updated_at': updated_at})
 
-def delete_note(event, context):
+def delete_resource(event, context):
     user_id = event['requestContext']['authorizer']['claims']['sub']
-    note_id = event['pathParameters']['note_id']
-    notes_table.delete_item(
+    resource_id = event['pathParameters']['resource_id']
+    resources_table.delete_item(
         Key={
             'user_id': user_id,
-            'note_id': note_id
+            'resource_id': resource_id
         }
     )
     return _make_response()
