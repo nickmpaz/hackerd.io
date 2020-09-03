b5b71f17678413993897d9178e4c50eeda761153
Nicholas Paz <nicholasmpaz@gmail.com>
2020-09-03 12:32:36 -0700

    change select styles

--- frontend/src/components/NamespaceSelectorDialog.vue
+++ frontend/src/components/NamespaceSelectorDialog.vue
@@ -2,8 +2,8 @@
   <div class="text-center">
     <v-dialog v-model="active" persistent width="500">
       <v-card color="secondary" class="px-8" :dark="$vuetify.theme.dark">
-        <div class="title py-4">Select Namespace</div>
-        <v-select :items="items" v-model="selected" label="Namespaces" solo></v-select>
+        <div class="title pt-4">Select Namespace</div>
+        <v-select :items="items" v-model="selected" label="Namespaces" single-line ></v-select>
         <div class="d-flex justify-space-between pb-6 pt-2">
           <v-btn width="150" color="success" @click="confirm">{{ confirmMessage }}</v-btn>
           <v-btn width="150" color="error" @click="decline">{{ declineMessage }}</v-btn>
