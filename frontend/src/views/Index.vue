b6da6a0bf088e674234edba53fdb78525130c055
Nicholas Paz <nicholasmpaz@gmail.com>
2020-09-08 17:31:32 -0700

    update

--- frontend/src/views/Index.vue
+++ frontend/src/views/Index.vue
@@ -316,7 +316,6 @@ export default {
       focused.scrollIntoView({
         behavior: "auto",
         block: "center",
-        inline: "center",
       });
     },
     incrementFocus: function () {
